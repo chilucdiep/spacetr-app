33754f8028f15a814a2ad93a7cc3ca829495a2f5
Chi-Luc <chilucdiep@hotmail.com>
2022-01-06 11:47:05 -0500

    APIFetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useCallback } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import styles from "./App.module.scss";
 
@@ -5,12 +6,37 @@ import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./components/LandingPage/LandingPage";
 
 function App() {
+  const APOD_URL = "https://api.nasa.gov/planetary/apod";
+  const API_KEY = "6A0tZQBiVWL7Tgahg0FxdeTB6DSUf6cMcy5QxMRy";
+
+  const [pictures, setPictures] = useState<any[]>([]);
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  const getPicturesRequest = useCallback (async () => {
+    const url = `${APOD_URL}?api_key=${API_KEY}&count=9`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson) {
+      setPictures(pictures => [...pictures, ...responseJson]);
+      setLoaded(!loaded);
+    } else {
+      console.log('error');
+    }
+  }, [loaded]);
+
+  useEffect(() => {
+    getPicturesRequest();
+  }, [getPicturesRequest]);
+
+  console.log(pictures);
+
   return (
     <BrowserRouter>
       <div className={styles.Container}>
         <Navbar />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage pictures={pictures} />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,11 +1,18 @@
-import styles from './LandingPage.module.scss'
+import styles from "./LandingPage.module.scss";
 
-const LandingPage = () => {
-    return (
-        <div className={styles.LandingPage}>
-            <h1>Discover our <span>universe</span></h1>
-        </div>
-    )
-}
+const LandingPage = ({ pictures }: any) => {
+  return (
+    <div className={styles.LandingPage}>
+      <h1>
+        Discover our <span>universe</span>
+      </h1>
+      <div>
+        {pictures.map((picture: any) => (
+          <img src={picture.url} className={styles.Picture}></img>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
