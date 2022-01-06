dd4612b0e126b7d8b36c0e8b6eb7a1123826c524
Chi-Luc <chilucdiep@hotmail.com>
2022-01-05 23:00:05 -0500

    Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+// import styles from "./Navbar.module.scss";
+
+// import Logo from "../../images/Logo.svg";
+// import Logo from '../../images/Logo.svg'
+
+const Navbar = () => {
+    return (
+        <div>
+            oki
+        {/* <img src={Logo} alt="Logo"></img> */}
+        </div>
+    )
+}
+
+export default Navbar
