b72dad615cfd4d125d8d6aa683908bb2b88e0003
Chi-Luc <chilucdiep@hotmail.com>
2022-01-20 20:22:57 -0500

    Infinite scroll for feed

--- src/components/FeedPage/Feed/Feed.tsx
+++ src/components/FeedPage/Feed/Feed.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useCallback } from "react";
+import { useInView } from "react-intersection-observer";
+import { TailSpin } from "react-loader-spinner";
 import usePictures from "../../../hooks/usePictures";
 import styles from "./Feed.module.scss";
 
@@ -10,18 +13,44 @@ interface FeedProps {
 }
 
 function Feed({ likedPictures, setLikedPictures }: FeedProps) {
-  const { pictures } = usePictures(8);
+  const { pictures, loading, error, fetchMore } = usePictures(8);
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      fetchMore();
+    }
+  }, [inView]);
 
   return (
     <div className={styles.Feed}>
-      {pictures?.map((picture: Picture, key: number) => (
-        <Card
-          picture={picture}
-          likedPictures={likedPictures}
-          setLikedPictures={setLikedPictures}
-          key={key}
-        />
-      ))}
+      <div className={styles.Pictures}>
+        {pictures?.map((picture: Picture, index: number) =>
+          pictures.length === index + 1 ? (
+            <div ref={ref}>
+              <Card
+                picture={picture}
+                likedPictures={likedPictures}
+                setLikedPictures={setLikedPictures}
+                key={picture.date}
+              />
+            </div>
+          ) : (
+            <Card
+              picture={picture}
+              likedPictures={likedPictures}
+              setLikedPictures={setLikedPictures}
+              key={picture.date}
+            />
+          )
+        )}
+      </div>
+      <div className={styles.NoData}>
+        {loading && <TailSpin color="#357cf2" height={30} width={30} />}
+        {error && <p>Error fetching the pictures. Please refresh the page.</p>}
+      </div>
     </div>
   );
 }
