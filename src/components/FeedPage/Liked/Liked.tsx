56e8ba29e4f74c69eb841ed5f67fd9f31a6613bc
chiludiep <chiludiep@hotmail.com>
2023-02-17 17:57:47 -0500

    feed anim

--- src/components/FeedPage/Liked/Liked.tsx
+++ src/components/FeedPage/Liked/Liked.tsx
@@ -1,3 +1,5 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { duration } from "moment";
 import { Link } from "react-router-dom";
 
 import { Picture } from "../../../types/Interfaces";
@@ -8,9 +10,20 @@ interface LikedProps {
 
 export default function Liked({ likedPictures }: LikedProps) {
   const likedPicturesMarkup = likedPictures ? (
-    <div className={styles.LikedPictures}>
+    <motion.div
+      className={styles.LikedPictures}
+      initial={{ opacity: 1, y: 10 }}
+      animate={{ opacity: 1, y: 0, transition: { duration: 0.3 } }}
+      exit={{ opacity: 0, y: 10, transition: { duration: 0.3 } }}
+    >
       {likedPictures.map((picture: Picture) => (
-        <div className={styles.LikedPicture} key={picture.date}>
+        <motion.div
+          className={styles.LikedPicture}
+          key={picture.date}
+          initial={{ opacity: 1, y: 10 }}
+          animate={{ opacity: 1, y: 0, transition: { duration: 0.3 } }}
+          exit={{ opacity: 0, y: 10, transition: { duration: 0.3 } }}
+        >
           <Link to={`/feed/${picture.date}`}>
             <img src={picture.url} alt="Nasa Space Imagery"></img>
           </Link>
@@ -20,14 +33,20 @@ export default function Liked({ likedPictures }: LikedProps) {
             </Link>
             <p>{picture.date}</p>
           </div>
-        </div>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   ) : null;
 
   const hasLikedPicturesMarkup =
     likedPictures.length === 0 ? (
-      <h2>No picture liked</h2>
+      <motion.h2
+        initial={{ opacity: 1, y: 10 }}
+        animate={{ opacity: 1, y: 0, transition: { duration: 0.3 } }}
+        exit={{ opacity: 0, y: 10, transition: { duration: 0.3 } }}
+      >
+        No picture liked
+      </motion.h2>
     ) : (
       likedPicturesMarkup
     );
@@ -35,7 +54,7 @@ export default function Liked({ likedPictures }: LikedProps) {
   return (
     <section className={styles.Liked}>
       <h4>Pictures you've liked</h4>
-      {hasLikedPicturesMarkup}
+      <AnimatePresence>{hasLikedPicturesMarkup}</AnimatePresence>
     </section>
   );
 }
