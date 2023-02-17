9532e329703b59ead7a00fb4a25d82b703604add
chiludiep <chiludiep@hotmail.com>
2023-02-17 17:46:22 -0500

    landing anim

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -6,11 +6,44 @@ import Button from "../Button/Button";
 import usePictures from "../../hooks/usePictures";
 import { Picture, Theme } from "../../types/Interfaces";
 import styles from "./LandingPage.module.scss";
+import { motion } from "framer-motion";
 
 export default function LandingPage({ lightTheme, setLightTheme }: Theme) {
   setLightTheme(false);
-
   const { pictures } = usePictures(5);
+
+  const containerVariant = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.9,
+      },
+    },
+  };
+
+  const listItemVariant = {
+    hidden: { y: 50, opacity: 0 },
+    show: {
+      y: 10,
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  const textVariant = {
+    hidden: { y: 20, x: 10, opacity: 0 },
+    show: {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.6,
+      },
+    },
+  };
 
   return (
     <>
@@ -18,7 +51,10 @@ export default function LandingPage({ lightTheme, setLightTheme }: Theme) {
       <section className={styles.LandingPage}>
         <section className={styles.Header}>
           <h1>
-            Discover our <span>universe</span>
+            Discover our{" "}
+            <motion.span variants={textVariant} initial="hidden" animate="show">
+              universe
+            </motion.span>
           </h1>
           <h2>
             with <strong>NASA's</strong> beautiful imagery
@@ -29,17 +65,26 @@ export default function LandingPage({ lightTheme, setLightTheme }: Theme) {
         </section>
         <section className={styles.Bottom}>
           <div className={styles.Overlay}></div>
-          <div className={styles.Images}>
-            {pictures?.map((picture: Picture) => (
-              <Link to={`/feed/${picture.date}`}>
-                <img
-                  src={picture.hdurl}
-                  alt="Nasa Imagery"
+          {pictures && (
+            <motion.div
+              className={styles.Images}
+              variants={containerVariant}
+              initial="hidden"
+              animate="show"
+            >
+              {pictures.map((picture: Picture) => (
+                <motion.div
+                  className={styles.Image}
                   key={picture.date}
-                ></img>
-              </Link>
-            ))}
-          </div>
+                  variants={listItemVariant}
+                >
+                  <Link to={`/feed/${picture.date}`}>
+                    <img src={picture.hdurl} alt="Nasa Imagery"></img>
+                  </Link>
+                </motion.div>
+              ))}
+            </motion.div>
+          )}
         </section>
       </section>
     </>
