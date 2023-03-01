cddc8768509e93448f7e0ea8c7fb49ed583d949e
chiludiep <chiludiep@hotmail.com>
2023-02-28 22:23:47 -0500

    hubble anim

--- src/components/HubbleDetailsPage/HubbleDetailsPage.tsx
+++ src/components/HubbleDetailsPage/HubbleDetailsPage.tsx
@@ -5,6 +5,57 @@ import styles from "./HubbleDetailsPage.module.scss";
 import useHubblePicture from "../../hooks/useHubblePicture";
 import useAstrologicalSign from "../../hooks/useAstrologicalSign";
 import useHubbleAIPrompt from "../../hooks/useHubbleAIPrompt";
+import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+
+const headerHello = {
+  hidden: { y: 20, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+const headerTitle = {
+  hidden: { y: 20, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      delay: 0.2,
+    },
+  },
+};
+
+const headerCaption = {
+  hidden: { y: 20, opacity: 0 },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      delay: 0.4,
+    },
+  },
+};
+
+const messageBox = {
+  hidden: { x: 120, y: 120, scale: 0.3, opacity: 0 },
+  show: {
+    scale: 1,
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+      delay: 1,
+    },
+  },
+};
 
 export default function HubbleDetailsPage({
   lightTheme,
@@ -24,34 +75,67 @@ export default function HubbleDetailsPage({
 
   const headerMarkup = (
     <section className={styles.Header}>
-      <h5>Hello little {signName},</h5>
-      <h1>Discover Your Birthday's Cosmic Connection!</h1>
-      <h3>
+      <motion.h5 variants={headerHello} initial="hidden" animate="show">
+        Hello little {signName},
+      </motion.h5>
+      <motion.h1 variants={headerTitle} initial="hidden" animate="show">
+        Discover Your Birthday's Cosmic Connection!
+      </motion.h1>
+      <motion.h3 variants={headerCaption} initial="hidden" animate="show">
         Hubble Space Telescope has been capturing awe-inspiring images of the
         cosmos since its launch in 1990. Our AI-generated message for you is
         personalized and full of fun references to astrology. So, get ready to
         blast off into the stars and explore Hubble's birthday photo on your
         special day!
-      </h3>
+      </motion.h3>
     </section>
   );
 
   const personalizedMessage = (
-    <section className={styles.PersonalizedMessage}>
+    <motion.section
+      className={styles.PersonalizedMessage}
+      variants={messageBox}
+      initial="hidden"
+      animate="show"
+    >
       <h2>Our personalized words of the day to you</h2>
       <div>
         <p className={styles.Caption}>
-          {loading ? "ChatGPT is cooking, let him cook..." : personalizedText}
+          {loading ? (
+            <Typewriter
+              options={{
+                strings: ["", "ChatGPT is cooking...", "Let him cook."],
+                autoStart: true,
+                loop: true,
+                delay: 40,
+                deleteSpeed: 30,
+              }}
+            />
+          ) : (
+            <Typewriter
+              options={{
+                strings: [personalizedText ? personalizedText : ""],
+                autoStart: true,
+                delay: 40,
+                deleteSpeed: 30,
+              }}
+            />
+          )}
         </p>
         <h6>
           {signName === "Virgo" && !loading ? "Virgos are toxic btw" : ""}
         </h6>
       </div>
-    </section>
+    </motion.section>
   );
 
   const pictureMarkup = picture ? (
-    <section className={styles.Picture}>
+    <motion.section
+      className={styles.Picture}
+      variants={headerHello}
+      initial="hidden"
+      animate="show"
+    >
       <div className={styles.Info}>
         <h1>{picture.Name}</h1>
         <p className={styles.Date}>{picture.Date}</p>
@@ -62,7 +146,7 @@ export default function HubbleDetailsPage({
         src={`https://imagine.gsfc.nasa.gov/hst_bday/images/${picture.Image}`}
         alt="Hubble pic"
       />
-    </section>
+    </motion.section>
   ) : null;
 
   return (
